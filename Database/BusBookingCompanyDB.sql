9e7fbaa73e5de3cc2e94a9e9afa17959b478fb41
Muhammad-saad-2000 <mohamedmiogh2@gmail.com>
2020-12-29 00:53:04 +0200

    add DB

---
+++ Database/BusBookingCompanyDB.sql
@@ -0,0 +1,145 @@
+create database BusBookingCompany
+Go
+use BusBookingCompany
+
+create table station
+(
+	id int NOT NULL,
+	location varchar(50),
+	Primary key(id),
+)
+
+create table bookingEmployee
+(
+	id int NOT NULL,
+	firstName varchar(50) NOT NULL,
+	lastName varchar(50) NOT NULL,
+	gender char Not null,
+	homeAddress varchar(50),
+	salary int ,
+	username varchar(30) NOT NULL,
+	userpassword varchar(30) NOT NULL,
+	stationId int,
+	Primary key(id),
+	Foreign key (stationId) references station (id)
+	ON DELETE SET NULL
+	ON UPDATE CASCADE,
+)
+
+create table analyst
+(
+	id int NOT NULL,
+	firstName varchar(50) NOT NULL,
+	lastName varchar(50) NOT NULL,
+	gender char NOT NULL,
+	homehomeAddress varchar(50),
+	salary int,
+	username varchar(30) NOT NULL,
+	userpassword varchar(30)NOT NULL,
+	stationId int,
+	Primary key(id),
+	Foreign key (stationId) references station (id)
+	ON DELETE set NULL 
+	ON UPDATE CASCADE,
+)
+
+create table paymentCard
+(
+	id int not null,
+	moneyAmount int not null,
+	primary key (id)
+)
+
+create table trip
+(
+	id int,
+	destination varchar(50) not null,
+	startingDate date,
+	duration int not null,
+	cost int not null,
+	primary key (id),
+)
+
+create table Passenger
+(
+	id int not null,
+	firstName varchar(50) NOT NULL,
+	lastName varchar(50) NOT NULL,
+	gender char Not null,
+	phone varchar(50),
+	username varchar(50) not null,
+	userpassword varchar(50) not null ,
+	tripId int ,
+	cardId int not null,
+	primary key(id),
+	Foreign key(tripId) references trip
+	ON DELETE set NULL
+	on UPDATE cascade,
+	Foreign key (cardId) references paymentCard
+	ON DELETE cascade
+	on UPDATE cascade,
+)
+
+create table driver
+(
+	id int not null,
+	firstName varchar(50) NOT NULL,
+	lastName varchar(50) NOT NULL,
+	gender char Not null,
+	homeAddress varchar(50),
+	salary int,
+	username varchar(50) not null,
+	userpassword varchar(50) not null,
+	rate float,
+	primary key (id),
+)
+
+create table mechanic
+(
+	id int not null ,
+	firstName varchar(50) NOT NULL,
+	lastName varchar(50) NOT NULL,
+	gender char Not null,
+	homeAddress varchar(50),
+	salary int,
+	username varchar(50) not null,
+	userpassword varchar(50) not null,
+	primary key (id),
+)
+
+
+create table vehicle
+(
+	id int not null,
+	model varchar(50) not null,
+	vehicleType varchar(50) not null,
+	needToReapair BIT not null,
+	no_currentPassengers int,
+	no_passengers int not null,
+	tripId int,
+	stationId int,
+	driverId int,
+	primary key (id),
+	Foreign key (tripId) references trip 
+	ON DELETE set NULL
+	on UPDATE cascade,
+	Foreign key (stationId) references station
+	ON DELETE set NULL
+	on UPDATE CASCADE,
+	Foreign key (driverId) references driver
+	ON DELETE set NULL
+	on UPDATE cascade,
+)
+
+create table fix
+(
+	vechicleId int,
+	mechanicId int,
+	primary key (vechicleId,mechanicId),
+	Foreign key (vechicleId) references vehicle
+	ON DELETE cascade
+	on UPDATE cascade,
+	Foreign key (mechanicId) references mechanic
+	ON DELETE cascade
+	on UPDATE cascade,
+)
---
+++ Database/addData.sql
@@ -0,0 +1,35 @@
+Go
+use BusBookingCompany
+
+insert into station
+values
+	(046878, 'california'),
+	(277286, 'cairo')
+
+insert into bookingEmployee
+values
+	(300625, 'Monserrat', 'Kautzer', 'm', '30434 Johns Square Apt. 945', 1000, 'vella19', 'fj80u6FCaJ', 046878)
+insert into analyst
+values
+	(317883, 'Britney', 'Flatley', 'f', '3367 Rollin Skyway', 2000, 'mayeschowalter', 'jXZiKUMpWb', 046878)
+insert into paymentCard
+values
+	(322157, 100)
+insert into trip
+values
+	(601871, 'florida', '2020-10-10', 3, 100)
+insert into Passenger
+values
+	(540413, 'Stan', 'Kris', 'm', '+201109338669', 'effertzprice', 'fAI9P2ZE4h', 601871, 322157)
+insert into driver
+values
+	(011708, 'Drake', 'Kunze', 'm', '3352 Hoppe Ville', 1200, 'cynthia12', 'fBUu8Kl7o', 3.5)
+insert into mechanic
+values
+	(204796, 'Tony', 'Klocko', 'm', '27790 Kareem Dale Apt. 881', 1000, 'vbins', 'PkuEMuyKv')
+insert into vehicle
+values
+	(725864, 'mercides', 'super jet', 0, 10, 30, 601871, 046878, 011708)
+insert into fix
+values
+	(725864, 204796)
