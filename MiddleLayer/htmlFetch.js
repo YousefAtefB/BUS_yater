687e3c38f446ca173726219001281ca55a078c5e
Muhammad-saad-2000 <mohamedmiogh2@gmail.com>
2021-01-01 18:36:11 +0200

    additions

--- MiddleLayer/htmlFetch.js
+++ MiddleLayer/htmlFetch.js
@@ -17,3 +17,46 @@ async function sendLogInInfo() {
 	console.log(resivedData);
 	//-------------------------------
 }
+async function mechanicFixedTheVechile() {
+	//data to be sent
+	let dataToSend = {
+		id: 121211241, //id of the bus that fixed
+	};
+	//--------------------------------
+	let response = await fetch(
+		'http://127.0.0.1:8080/mechanicFixedTheVechile',
+		{
+			method: 'POST',
+			body: JSON.stringify(dataToSend),
+			headers: {
+				'Content-Type': 'application/json',
+			},
+		}
+	);
+	let resivedData = await response.json();
+	//display the data
+	console.log(resivedData);
+	//-------------------------------
+}
+
+async function vechilesThatMechanicFixes() {
+	//data to be sent
+	let dataToSend = {
+		id: 121211241, //id of the mechani
+	};
+	//--------------------------------
+	let response = await fetch(
+		'http://127.0.0.1:8080/mechanicAssigendVechile',
+		{
+			method: 'POST',
+			body: JSON.stringify(dataToSend),
+			headers: {
+				'Content-Type': 'application/json',
+			},
+		}
+	);
+	let resivedData = await response.json();
+	//display the data
+	console.log(resivedData);
+	//-------------------------------
+}
