cde9be4d6f776cc073fb161804ce245a4a11481a
Ahmed-Emad10 <65132004+Ahmed-Emad10@users.noreply.github.com>
2021-01-06 18:19:15 +0200

    new driver

--- Driver/js/main.js
+++ Driver/js/main.js
@@ -1,12 +1,70 @@
 var vehicleId=document.getElementById("idspan"),
-    vehicleType=document.getElementById("tspan"),
-    BTN=document.getElementById("btn");
+    BTN=document.getElementById("btn"),
+	arr;
+vehicleId.innerHTML=123456;
 
-vehicleId.innerHTML="#123456";
-vehicleType.innerHTML="MCV";
 
-BTN.onclick = function() {
+/*BTN.onclick = function() {
     "use strict";
     var id = vehicleId.innerText;
     console.log(id);
+}*/
+
+async function lastSignedIn() {
+	//data to be sent
+	//nothing
+	//--------------------------------
+	let response = await fetch('http://127.0.0.1:8080/lastSignedIn');
+	let resivedData = await response.json();
+	//display the data
+	arr=resivedData;
+	//------------------
 }
+lastSignedIn();
+
+async function DriverVehicle() {
+	//data to be sent
+	let dataToSend = {
+		username: arr["username"]   //driver username
+	};
+	//--------------------------------
+	let response = await fetch(
+		'http://127.0.0.1:8080/DriverVehicle',
+		{
+			method: 'POST',
+			body: JSON.stringify(dataToSend),
+			headers: {
+				'Content-Type': 'application/json',
+			},
+		}
+	);
+	let resivedData = await response.json();
+    //display the data
+    arr=resivedData;                      //EDITED
+	vehicleId.innerHTML=arr["id"];         //EDITED
+	//-------------------------------
+}
+
+DriverVehicle();
+
+BTN.onclick =async function DriverToBeRepaired() {
+	//data to be sent
+	let dataToSend = {
+		id : vehicleId.innerText  //bus id EDITED
+	};
+	//--------------------------------
+	let response = await fetch(
+		'http://127.0.0.1:8080/DriverToBeRepaired',
+		{
+			method: 'POST',
+			body: JSON.stringify(dataToSend),
+			headers: {
+				'Content-Type': 'application/json',
+			},
+		}
+	);
+	let resivedData = await response.json();
+	//display the data
+	console.log(resivedData);
+	//-------------------------------
+}*/
